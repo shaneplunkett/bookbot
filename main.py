89c6385cdeb1d6d22016c0d3fa572c84f3e2a47d
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-02-23 02:05:15 +1100

    Refactor for course update

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-def main():
-    frankenstein = "books/frankenstein.txt"
-    print(get_file_content(frankenstein))
-    print(get_word_count(get_file_content(frankenstein)))
-    print(get_char_count(get_file_content(frankenstein)))
-    print(f"--- Begin report of {frankenstein} ---")
-    print()
-    print(f"{get_word_count(get_file_content(frankenstein))} words found in document")
-    print()
-    print(char_report(get_char_count(get_file_content(frankenstein))))
-    print()
-    print(f"--- End report ---")
+import sys
 
+from stats import get_num_words
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <file_path>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(get_file_content(file_path))} total words")
+    print("--------- Character Count -------")
+    char_report(get_char_count(get_file_content(file_path)))
+    print("============= END ===============")
 
 
 def get_file_content(path):
     with open(path) as f:
         return f.read()
-
-
-def get_word_count(string):
-    words = string.split()
-    return len(words)
 
 
 def get_char_count(string):
@@ -45,7 +45,7 @@ def char_report(char_count):
             list_of_dics.append(char_dict)
     list_of_dics.sort(reverse=True, key=lambda x: x["count"])
     for dic in list_of_dics:
-        print(f"The {dic['char']} was found {dic['count']} times")
+        print(f"{dic['char']}: {dic['count']}")
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(string):
+    words = string.split()
+    return len(words)
